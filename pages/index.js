2b9095e20c110c83047034b586466189b2893e50
Ruben Marcus <ruben@rubenmarcus.dev>
2022-07-07 04:29:50 +0100

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,52 @@
-import Layout from '../components/layout'
-import { TypeText } from '../components/type'
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
     <Layout>
       <section>
         <article>
-        <p className="blue-text">About</p>
-        <p>Hello, my name is Ruben Marcus, and I´m a 10 year experienced <br/>Senior Software engineer from Brazil.</p>
-        <p>I´m Front-end specialist,with experience in React,Angular,Vue,Redux and etc.</p>
-        <p>Also have knowledge in Software Architecture,TDD,CI/CD,<br/>Backend development.</p>
+          <p className="blue-text">About</p>
+          <p>
+            Hello, my name is Ruben Marcus, and I´m a 10 year experienced <br />
+            Senior Software Engineer based in Lisbon - Portugal.
+          </p>
+          <p>you can download my resume</p>
+          <p>or hire me</p>
         </article>
         <article>
-        <p className="blue-text"> Latest blog post: </p>
-        <h2><a href="https://dev.to/rubenmarcus/why-use-next-js-strapi-16b1">Why use Next.js + Strapi?</a></h2>
+          <p className="blue-text"> Latest blog post: </p>
+          <h2>
+            <a href="https://dev.to/rubenmarcus/getting-started-with-next-js-strapi-security-first-3380">
+              Getting started with Next.js + Strapi: Security first
+            </a>
+          </h2>
         </article>
-      
       </section>
-      <aside>       
-      <img className="me" src="./me.jpg" />
-      <h1>rubenmarcus.dev</h1>
-      <span className="blue-text">Senior Software Engineer</span>
-      <div className="socialLinks">
-      <a href="https://github.com/rubenmarcus"><img src="./github.svg" /></a>
-      <a href="https://dev.to/rubenmarcus"><img src="./dev.svg" /></a>
-      <a href="https://rubenmarcus.medium.com/"><img src="./medium.svg" /></a>
-      <a href="https://stackoverflow.com/users/5905497/ruben-marcus"><img src="./stack.svg" /></a>
-      <a href="https://linkedin.com/in/rubenmarcus"><img src="./linkedin.svg" /></a>
-      <a href="mailto:ruben@rubenmarcus.dev"><img src="./mail.svg" /></a>
-      </div>
-     </aside>
+      <aside>
+        <img width="80%" className="me" src="./me.jpg" />
+        <h1>Ruben Marcus</h1>
+        <span className="blue-text">Senior Software Engineer</span>
+        <div className="socialLinks">
+          <a href="https://github.com/rubenmarcus">
+            <img src="./icon-github.svg" />
+          </a>
+          <a href="https://dev.to/rubenmarcus">
+            <img src="./dev.svg" />
+          </a>
+          <a href="https://rubenmarcus.medium.com/">
+            <img src="./medium.svg" />
+          </a>
+          <a href="https://stackoverflow.com/users/5905497/ruben-marcus">
+            <img src="./stack.svg" />
+          </a>
+          <a href="https://linkedin.com/in/rubenmarcus">
+            <img src="./linkedin.svg" />
+          </a>
+          <a href="mailto:ruben@rubenmarcus.dev">
+            <img src="./mail.svg" />
+          </a>
+        </div>
+      </aside>
     </Layout>
-  )
-}
-
-const clientType = () => {
-  if (process.browser) {
-    return  <TypeText />
-  } 
-  return ''
+  );
 }
